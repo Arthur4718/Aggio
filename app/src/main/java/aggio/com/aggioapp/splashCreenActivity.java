a41572f56e67607e1e38571494f0e2950c0f3bef
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-09 01:24:01 -0300

    ImageView click.
    Added method to open the login activity.

--- app/src/main/java/aggio/com/aggioapp/splashCreenActivity.java
+++ app/src/main/java/aggio/com/aggioapp/splashCreenActivity.java
@@ -2,12 +2,27 @@ package aggio.com.aggioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class splashCreenActivity extends AppCompatActivity {
 
+    private ImageView splashImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_creen);
+        getSupportActionBar().hide();
+
+        splashImage = (ImageView) findViewById(R.id.splashImageView);
+
+        splashImage.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               
+                                           }
+                                       }
+        );
     }
 }
