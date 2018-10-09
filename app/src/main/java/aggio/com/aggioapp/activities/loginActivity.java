3143447feb3786d987369e3bcee32e5e643c9916
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-09 01:28:17 -0300

    Login Activity..
    Added a new login Activity.

---
+++ app/src/main/java/aggio/com/aggioapp/activities/loginActivity.java
@@ -0,0 +1,16 @@
+package aggio.com.aggioapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import aggio.com.aggioapp.R;
+
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
